1c2ec03ccfd59c3c73e254830696c8027b775acc
Sidia <sidia.reis1@gmail.com>
2022-01-22 19:20:33 -0800

    add Log Level messages

--- app/src/main/java/com/example/debuggingapp/MainActivity.kt
+++ app/src/main/java/com/example/debuggingapp/MainActivity.kt
@@ -7,7 +7,14 @@ import android.util.Log
 private const val TAG = "MainActivity"
 
 fun logging() {
-    Log.v(TAG, "Hello, world!")
+    fun logging() {
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+    }
+
 }
 
 class MainActivity : AppCompatActivity() {
